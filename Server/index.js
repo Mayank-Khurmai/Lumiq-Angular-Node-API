1ae8dbb45ef9b734ccf1ece0b12081cfca1c4d73
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2022-03-03 23:34:43 +0530

    signup server side code with MongoDB

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,8 @@
 const http  = require("http");
-const query = require('querystring');
+const mongo = require("mongodb").MongoClient;
 
 const server = http.createServer((request, response)=>{
-
+        // Receiving Data
         var incoming_data = "";
         request.on('data', (chunks)=>{
             incoming_data += chunks.toString();
@@ -10,7 +10,7 @@ const server = http.createServer((request, response)=>{
 
         request.on('end', ()=>{
             incoming_data = JSON.parse(incoming_data);
-            let data = {
+            let receivedData = {
                 'name' : incoming_data.name,
                 'email' : incoming_data.email,
                 'mobile' : incoming_data.mobile,
@@ -18,14 +18,52 @@ const server = http.createServer((request, response)=>{
                 'gender' : incoming_data.gender,
                 'password' : incoming_data.password
             }
+
             
-        response.writeHead(200, {
-            'Content-type' : 'application/json',
-            'Access-Control-Allow-Origin' : '*'
+        // Connection with MongoDB
+        
+        var mongo_url = "mongodb://127.0.0.1:27017";
+        mongo.connect(mongo_url, (error, conn)=>{
+
+            const db = conn.db("api_db");
+            db.createCollection("loginDetails", (error, conn)=>{
+
+                let dataCheck = {
+                    'email': receivedData.email
+                };
+                db.collection("loginDetails").find(dataCheck).toArray((error, data)=>{
+                    if(data.length != 0){
+                        response.writeHead(409, {
+                            'Content-type' : 'application/json',
+                            'Access-Control-Allow-Origin' : '*'
+                        });
+                        let outputMsg = {
+                            message: "Already Exists"
+                          };
+                        response.write(JSON.stringify(outputMsg));
+                        response.end(); 
+                    }
+                    else{
+                        db.collection("loginDetails").insertOne(receivedData, (error, conn)=>{
+                            response.writeHead(200, {
+                                'Content-type' : 'application/json',
+                                'Access-Control-Allow-Origin' : '*'
+                            });
+                            let outputMsg = {
+                                message: "Inserted"
+                              };
+                            response.write(JSON.stringify(outputMsg));
+                            response.end(); 
+                        });
+                    }
+                });
+
+            });
+
         });
-        response.write(JSON.stringify(data));
-        response.end(); 
-        });
+                
+    });
+
 });
 
 server.listen(8080);
