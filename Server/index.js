5c1d5e94825943376b31c73951e9c5a435190942
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2022-03-03 22:37:46 +0530

    Receiving POST data

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,31 @@
 const http  = require("http");
+const query = require('querystring');
 
 const server = http.createServer((request, response)=>{
 
+        var incoming_data = "";
+        request.on('data', (chunks)=>{
+            incoming_data += chunks.toString();
+        });
+
+        request.on('end', ()=>{
+            incoming_data = JSON.parse(incoming_data);
             let data = {
-                'name' : 'mayank',
-                'email' : 'mkk@gmail.com',
-                'mobile' : '123',
-                'gender' : 'male',
-                'password' : 'abc'
+                'name' : incoming_data.name,
+                'email' : incoming_data.email,
+                'mobile' : incoming_data.mobile,
+                'address' : incoming_data.address,
+                'gender' : incoming_data.gender,
+                'password' : incoming_data.password
             }
+            
         response.writeHead(200, {
             'Content-type' : 'application/json',
             'Access-Control-Allow-Origin' : '*'
         });
         response.write(JSON.stringify(data));
-        response.end();
+        response.end(); 
+        });
 });
 
 server.listen(8080);
